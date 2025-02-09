7a6bb64581a365f1290ed27a8c91150564cfd4eb
Masha212121 <masha21032006@yandex.ru>
2025-02-09 20:42:43 +0700

    Add files via upload

---
+++ №14.py
@@ -0,0 +1,3 @@
+number = int(input())
+formula = ((number+2)*3-6)/3-4
+print(formula)
---
+++ №8.py
@@ -0,0 +1,4 @@
+L=input('Login:')
+P=input('Password:')
+NP=input('New password:')
+print('User durov has changed the password to querty')
