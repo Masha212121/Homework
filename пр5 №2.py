28ec621e604fed8767bc2b542d66abcb88c648b2
Masha212121 <masha21032006@yandex.ru>
2025-03-16 22:23:41 +0700

    Update пр5 №2.py

--- пр5 №2.py
+++ пр5 №2.py
@@ -1,11 +1,9 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-
 def draw_circle(center, radius):
     circle = plt.Circle(center, radius, color='blue', fill=False)
     plt.gca().add_artist(circle)
-
 
 def check_point_position(center, radius, point):
     distance_squared = (point[0] - center[0]) ** 2 + (point[1] - center[1]) ** 2
@@ -19,7 +17,6 @@ def check_point_position(center, radius, point):
         return "точка располагается за пределами окружности"
 
 
-# Запрашиваем данные у пользователя
 center_x = float(input("Введите координату X центра окружности: "))
 center_y = float(input("Введите координату Y центра окружности: "))
 radius = float(input("Введите радиус окружности: "))
@@ -29,7 +26,6 @@ point_y = float(input("Введите координату Y точки: "))
 center = (center_x, center_y)
 point = (point_x, point_y)
 
-# Строим окружность и точку
 draw_circle(center, radius)
 plt.plot(point[0], point[1], 'ro')  # 'ro' - красная точка
 plt.xlim(center_x - radius - 1, center_x + radius + 1)
@@ -39,6 +35,5 @@ plt.grid()
 plt.title("Окружность и точка")
 plt.show()
 
-# Проверяем положение точки
 result = check_point_position(center, radius, point)
 print(result)
